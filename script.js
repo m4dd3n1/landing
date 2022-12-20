ee3dde6676040324bdc3c6c2ccace0eb56be0d76
Konstantin <118168039+m4dd3n1@users.noreply.github.com>
2022-12-21 00:14:16 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function onEntry(entry) {
+    entry.forEach(change => {
+      if (change.isIntersecting) {
+        change.target.classList.add('element-show');
+      }
+    });
+  }
+  let options = { threshold: [0.5] };
+  let observer = new IntersectionObserver(onEntry, options);
+  let elements = document.querySelectorAll('.start');
+  for (let elm of elements) {
+    observer.observe(elm);
+  }
+
+  function copytext(el) {
+    var $tmp = $("<textarea>");
+    $("body").append($tmp);
+    $tmp.val($(el).text()).select();
+    document.execCommand("copy");
+    $tmp.remove();
+}    
